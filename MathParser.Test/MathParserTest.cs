328adc06e24f8761af5dd003deab13f8b93f18f7
antropod <antropod@gmail.com>
2014-06-26 05:30:59 +0400

    Created MathParser project: a simple C# expression parser, that allows you to evaluate mathematical expressions like 1/x+y*log(z) in runtime.

---
+++ MathParser.Test/MathParserTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Antropod.MathParser;
+
+namespace expression_parset_test
+{
+    [TestClass]
+    public class MathParserTest
+    {
+        public bool ParseFails(string expression)
+        {
+            try 
+            {
+                MathParser.ParseExpression(expression);
+                return false;
+            }
+            catch(ParserException)
+            {
+                return true;
+            }
+        }
+
+        [TestMethod]
+        public void TestParseMutipleTimes()
+        {
+            var parser = new MathParser("1+2");
+            Assert.AreEqual(3, parser.Parse());
+            Assert.AreEqual(3, parser.Parse());
+        }
+
+        [TestMethod]
+        public void TestWitespace()
+        {
+            Assert.AreEqual(3, MathParser.ParseExpression(" ( 1 + 2) + log(1)"));
+        }
+
+
+        [TestMethod]
+        public void TestFloatingPoint()
+        {
+            Assert.AreEqual(1.0, MathParser.ParseExpression("1.0"));
+            Assert.AreEqual(1.1, MathParser.ParseExpression("1.1"));
+            Assert.AreEqual(123.0, MathParser.ParseExpression("123."));
+            Assert.AreEqual(123.456, MathParser.ParseExpression("123.456"));
+        }
+
+        [TestMethod]
+        public void TestFunctions()
+        {
+            Assert.AreEqual(42, MathParser.ParseExpression("id(42)"));
+            Assert.AreEqual(3, MathParser.ParseExpression("id(2+1)"));
+            Assert.AreEqual(49, MathParser.ParseExpression("id((1+(2*3))*(3+4))"));
+            Assert.AreEqual(1, MathParser.ParseExpression("id((((1))))"));
+            Assert.AreEqual(2, MathParser.ParseExpression("id(id(1+id(1)))"));
+        }
+
+        [TestMethod]
+        public void TestBuiltinFunctions()
+        {
+            Assert.AreEqual(Math.Log(42), MathParser.ParseExpression("log(42)"));
+            Assert.AreEqual(Math.Log10(42), MathParser.ParseExpression("log10(42)"));
+            Assert.AreEqual(Math.Abs(-322), MathParser.ParseExpression("abs(-322)"));
+            Assert.AreEqual(Math.Exp(-0.5), MathParser.ParseExpression("exp(-0.5)"));
+        }
+
+        [TestMethod]
+        public void TestVariables()
+        {
+            var parser = new MathParser("foo123");
+            parser.variables["foo123"] = 456;
+            Assert.AreEqual(456, parser.Parse());
+        }
+
+        [TestMethod]
+        public void TestParserExpression()
+        {
+            Assert.AreEqual(1, MathParser.ParseExpression("1"));
+            Assert.AreEqual(1, MathParser.ParseExpression("+1"));
+            Assert.AreEqual(-1, MathParser.ParseExpression("-1"));
+            Assert.AreEqual(-1, MathParser.ParseExpression("0-1"));
+            Assert.AreEqual(-2, MathParser.ParseExpression("0-1+2-3"));
+            Assert.AreEqual(6, MathParser.ParseExpression("2*3"));
+            Assert.AreEqual(2, MathParser.ParseExpression("4/2"));
+            Assert.AreEqual(21, MathParser.ParseExpression("(1+2)*(3+4)"));
+            Assert.AreEqual(49, MathParser.ParseExpression("(1+(2*3))*(3+4)"));
+            Assert.AreEqual(123, MathParser.ParseExpression("123"));
+
+            Assert.IsTrue(ParseFails("-"));
+            Assert.IsTrue(ParseFails("-1-"));
+            Assert.IsTrue(ParseFails("1+1+"));
+        }
+
+        [TestMethod]
+        public void TestCharToIntMethod()
+        {
+            var digits = "0123456789";
+            for (int x = 0; x < digits.Length; ++x )
+            {
+                Assert.AreEqual(x, MathParser.CharToInt(digits[x]));
+            }    
+        }
+    }
+}
---
+++ MathParser.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Antropod.MathParser
+{
+    /* 
+     * !! Может не соответствовать коду !!
+     * <number> = <digit><digit>*[.<digit>*]
+     * <variable> = <letter> (<letter> | <digit>)*
+     * <mulop> = "*" | "/"
+     * <addop> = "+" | "-"
+     * <expression> = [<addop>] <term> (<addop> <term>)*
+     * <factor> = "(" <expression> ")" | <variable> | <number>
+     * <term> = <factor> (<mulop> <factor>)*
+     */
+
+    public class ParserException: Exception
+    {
+        public ParserException(string message)
+            : base(message)
+        { }
+    }
+
+    public class MathParser
+    {
+        private string expression;
+        private int position;
+        private char look;
+        public readonly Dictionary<string, double> variables 
+            = new Dictionary<string, double>();
+        public delegate double Function(double x);
+        public readonly Dictionary<string, Function> functions 
+            = new Dictionary<string, Function>();
+
+        public void InitBuiltinFunctions()
+        {
+            functions["id"] = x => x;
+            functions["log"] = Math.Log;
+            functions["log10"] = Math.Log10;
+            functions["abs"] = Math.Abs;
+            functions["exp"] = Math.Exp;
+        }
+
+        public static int CharToInt(char c)
+        {
+            return Convert.ToInt32(c) - Convert.ToInt32('0');
+        }
+
+        private bool IsDigit(char c)
+        { 
+            return char.IsDigit(c); 
+        }
+
+        private bool IsAlpha(char c)
+        {
+            return char.IsLetter(c);
+        }
+
+        private static bool IsAddop(char c)
+        {
+            return "+-".Contains(c);
+        }
+
+        private bool IsMulop(char c)
+        {
+            return "*/".Contains(c);
+        }
+
+        private bool IsWhiteSpace(char c)
+        {
+            return Char.IsWhiteSpace(c);
+        }
+
+        private void Expected(string what)
+        {
+            throw new ParserException(what + " expected");
+        }
+
+        private void SkipWhiteSpace()
+        {
+            while (IsWhiteSpace(look))
+            {
+                GetChar();
+            }
+        }
+
+        private double GetNum()
+        {
+            string result = "";
+            if (!IsDigit(look)) Expected("Number");
+            while (IsDigit(look)) 
+            {
+                result += look;
+                GetChar();
+            }
+            if (look == '.')
+            {
+                result += look;
+                GetChar();
+
+                while (IsDigit(look))
+                {
+                    result += look;
+                    GetChar();
+                }
+            }
+            SkipWhiteSpace();
+            return double.Parse(result, CultureInfo.InvariantCulture.NumberFormat);
+        }
+
+        private string GetName()
+        {
+            string result = "";
+            if (!IsAlpha(look)) Expected("Name");
+            while (IsDigit(look) || IsAlpha(look))
+            {
+                result += look;
+                GetChar();
+            }
+            SkipWhiteSpace();
+            return result;
+        }
+
+        private char Read()
+        {
+            if (position < expression.Length)
+                return expression[position++];
+            return '\0';
+        }
+
+        private void GetChar()
+        {
+            look = Read();
+        }
+
+        private void Match(char c)
+        {
+            if (look == c)
+            {
+                GetChar();
+                SkipWhiteSpace();
+            }
+            else
+                Expected(string.Format("'{0}'", c));
+        }
+
+        public double Expression()
+        {
+            double result = 0;
+            if (IsAddop(look))
+                result = 0;
+            else
+                result = Term();
+
+            while (IsAddop(look))
+            {
+                switch (look)
+                {
+                    case '+': 
+                        Match('+');
+                        result += Term();
+                        break;
+                    case '-':
+                        Match('-');
+                        result -= Term();
+                        break;
+                    default:
+                        throw new Exception();
+                }
+
+            }
+            return result;
+        }
+
+        public double Term()
+        {
+            double result = Factor();
+            while (IsMulop(look))
+            {
+                switch (look)
+                {
+                    case '*':
+                        Match('*');
+                        result *= Factor();
+                        break;
+                    case '/':
+                        Match('/');
+                        result /= Factor();
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+            return result;
+        }
+
+        public double Factor()
+        {
+            double result;
+            if (look == '(')
+            {
+                Match('(');
+                result = Expression();
+                Match(')');
+            }
+            else if (IsAlpha(look))
+            {
+                var name = GetName();
+                if (look == '(')
+                {
+                    Match('(');
+                    result = functions[name]( Expression() );
+                    Match(')');
+                }
+                else
+                    result = variables[name];
+            }
+            else
+                result = GetNum();
+
+            return result;
+        }
+
+        public MathParser(string expression)
+        {
+            InitBuiltinFunctions();
+            this.expression = expression;
+            Reset();
+        }
+
+        public void Reset()
+        {
+            position = 0;
+            GetChar();
+            SkipWhiteSpace();
+        }
+
+        public double Parse()
+        {
+            Reset();
+            return Expression();
+        }
+
+        public static double ParseExpression(string expression)
+        {
+            return new MathParser(expression).Parse();
+        }
+    }
+}
